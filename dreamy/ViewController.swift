4ce3b32073200e2515502950392ccf27ddbedcca
jangjoonmo <jjmo97@ajou.ac.kr>
2023-03-24 21:33:24 +0900

    Initial Commit

---
+++ dreamy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dreamy
+//
+//  Created by 장준모 on 2023/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
